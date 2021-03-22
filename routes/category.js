296ff2385a219a4b8fcd0b8dffa4afc7745d6dda
Rupesh Kalra <rupeshkalra1999@gmail.com>
2021-03-22 23:31:38 +0530

    createCategory method added

--- routes/category.js
+++ routes/category.js
@@ -1,11 +1,15 @@
 const express =require("express");
 const router=express.Router();
 
-const {getCategoryById} =require("../controllers/category");
+const {getCategoryById,createCategory} =require("../controllers/category");
 const {getUserById} =require("../controllers/user");
 const {isAdmin,isSignedIn,isAuthenticated} =require("../controllers/auth");
 
+//Params
 router.param("userId",getUserById);
 router.param("categoryId",getCategoryById);
+
+//Routes
+router.post("/category/create/:userId",isSignedIn,isAuthenticated,isAdmin,createCategory);
 
 module.exports=router;
