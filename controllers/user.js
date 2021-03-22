6641533a340c9aa17c3c40643900744a64954619
Rupesh Kalra <rupeshkalra1999@gmail.com>
2021-03-22 19:21:16 +0530

    userPurchaList,Pushorder  added

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,5 @@
 const User =require("../models/user");
-
+const Order=require("../models/order");
 
 exports.getUserById =(req,res,next,id)=>
 {
@@ -42,4 +42,46 @@ exports.updateUser= (req,res)=>{
             res.send(user);
         }
      )
+}
+
+exports.userPurchaseList=(req,res)=>{
+    Order.find({user:req.profile._id})
+    .populate("user","_id name")
+    .exec((err,order)=>{
+        if(err){
+            res.status(400).json({
+                error:"No orders found!"
+            })
+        }
+        return res.json(order);
+    });
+}
+
+exports.pushOrderinPurchaseList=(req,res,next)=>{
+    let purchases=[];
+    req.body.order.products.forEach(product=>{
+        purchases.push({
+            _id:product._id,
+            name:product.name,
+            description:product.description,
+            category:product.category,
+            quantity:product.quantity,
+            amount:req.body.order.amount,
+            transaction_id:req.body.order.transaction_id
+        });
+    })
+
+    User.findOneAndUpdate(
+        {_id:req.profile._id},
+        {$push:{purchases:purchases}},
+        {new:true},
+        (err,purchases)=>{
+            if(err){
+                return res.status(400).json({
+                    error:"unable to save purchase list!!"
+                })
+            }
+            next();
+        }
+    )
 }
--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,9 @@ const {isAdmin,isAuthenticated,isSignedIn} = require("../controllers/auth");
 const {getUser,getUserById,updateUser} = require("../controllers/user");
 
 router.param("userId",getUserById);
+
 router.get("/user/:userId",isSignedIn,isAuthenticated,getUser);
 router.put("/user/:userId",isSignedIn,isAuthenticated,updateUser);
+router.get("/orders/user/:userId",isSignedIn,isAuthenticated,userPurchaseList);
 
 module.exports = router;
