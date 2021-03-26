e2cbe95f3d1371629d9b121436663b34cff6a76e
Rupesh Kalra <rupeshkalra1999@gmail.com>
2021-03-27 01:04:39 +0530

    createProduct added

--- controllers/product.js
+++ controllers/product.js
@@ -1,4 +1,8 @@
 const Product=require("../models/product");
+const formidable=require("formidable");
+const _ =require("lodash");
+const fs=require("fs");
+
 
 exports.getProductById=(req,res,next,id)=>{
     Product.findById(id)
@@ -11,6 +15,41 @@ exports.getProductById=(req,res,next,id)=>{
         }
         req.product=product;
         next();
+    })
+}
+
+exports.createProduct=(req,res)=>{
+    let form =formidable.IncomingForm();
+    form.keepExtensions=true;
+
+    form.parse(req,(err,fields,file)=>{
+        if(err){
+            return res.status(400).json({
+                error:"problem with image!"
+            })
+        };
+
+        
+        let product=new Product(fields);
+
+        if(file.photo){
+            if(file.photo.size>3000000){
+                return res.status(400).json({
+                    message:"Image size too large"
+                })
+            };
+            product.photo.data=fs.readFileSync(file.photo.path);
+            product.photo.contentType=file.photo.type;
+        }
+
+        product.save((err,product)=>{
+            if(err){
+                return res.status(400).json({
+                    error:"unable to create product"
+                })
+            };
+            res.json(product);
+        })
     })
 }
 
--- routes/product.js
+++ routes/product.js
@@ -3,10 +3,13 @@ const router =express.Router();
 
 const {isAdmin,isAuthenticated,isSignedIn}=require("../controllers/auth");
 const {getUserById}=require("../controllers/user");
-const {getProductById}=require("../controllers/product");
+const {getProductById,createProduct}=require("../controllers/product");
 
 //params
 router.param("userId",getUserById);
 router.param("productId",getProductById);
+
+//routes
+router.post("/product/create/:userId",isSignedIn,isAuthenticated,isAdmin,createProduct);
 
 module.exports=router;
