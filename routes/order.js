9841ce38c2ebf014634d81bd3fa59b089c259671
Rupesh Kalra <rupeshkalra1999@gmail.com>
2021-04-03 21:09:58 +0530

    createOrder added

--- routes/order.js
+++ routes/order.js
@@ -4,12 +4,14 @@ const router =express.Router();
 const {isAdmin,isAuthenticated,isSignedIn}=require("../controllers/auth");
 const {getUserById,pushOrderinPurchaseList}=require("../controllers/user");
 const {updateStock}=require("../controllers/product");
-const {getOrderById}=require("../controllers/order");
+const {getOrderById,createOrder}=require("../controllers/order");
 
 //params
 router.param("userId",getUserById);
 router.param("orderId",getOrderById);
 
 //actual routes
+router.post("/order/create/:userId",isSignedIn,isAuthenticated,pushOrderinPurchaseList,updateStock,createOrder);
+
 
 module.exports=router;
