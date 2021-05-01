c0a6f26e3fad268a674313b2b6762a7bd67a91e4
Rupesh Kalra <rupeshkalra1999@gmail.com>
2021-05-01 17:42:07 +0530

    basic payment setup

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const userRoutes =require("./routes/user");
 const categoryRoutes=require("./routes/category");
 const productRoutes=require("./routes/product");
 const orderRoutes=require("./routes/order");
+const paymentRoutes=require("./routes/paymentRoutes");
 
 //DB CONNECTION
 mongoose.connect(process.env.DATABASE,
@@ -34,6 +35,7 @@ app.use("/api",userRoutes);
 app.use("/api",categoryRoutes);
 app.use("/api",productRoutes);
 app.use("/api",orderRoutes);
+app.use("/api",paymentRoutes);
 
 //PORT
 const port = process.env.port || 8000;
